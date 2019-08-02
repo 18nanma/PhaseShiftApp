7ada2b355a0a9e187120cf9c6ad243393611e994
18nanma <42802604+18nanma@users.noreply.github.com>
2019-08-02 10:21:45 +0530

    Added Workshop, Schedule, Map, Events class

---
+++ Schedule.js
@@ -0,0 +1,77 @@
+
+import React, {Fragment} from 'react';
+import {
+  StatusBar,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  ImageBackground
+} from 'react-native';
+
+export default class Schedule extends React.Component {
+  render() {
+    return (
+      <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      <ScrollView
+          contentInsetAdjustmentBehavior="automatic">
+            <ImageBackground source={require('./src/images/BACKG5.png')} 
+            style={{
+              width: '100%',
+              flex:1,
+              height: '100%',
+              resizeMode: 'cover',
+              }}>
+              <View style={{
+                alignItems:'flex-end',
+                resizeMode: 'contain',
+                padding:10,
+                paddingRight:0,
+                paddingTop:3
+              }}>
+                <Image source={require('./src/images/ps_logo_white_2019-07-31/ps_logo_white.png')} />
+              </View>
+              
+              <View style={{
+                alignItems: 'center',
+              justifyContent: 'center'
+              }} >
+                  
+
+                <View style={{
+                    paddingBottom:45
+                }}>
+                    <Image source={require('./src/images/schedule_2019-08-02/schedule.png')} />
+                </View>
+
+                <View style={{
+                    padding:28,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <Text style={{
+                    letterSpacing:0.16,
+                    textAlign:'auto',
+                    fontStyle: 'Poppins-ExtraLight',
+                    paddingLeft: 9,
+                    fontSize: 18,
+                    color: '#fff',
+                    lineHeight: 28
+                    }}> Info will be updated soon......
+                            
+                        </Text>
+                    </View>
+               </View>
+
+                </ImageBackground>
+        </ScrollView>
+        </SafeAreaView>
+      </Fragment>
+        
+      
+    );
+  }
+}
